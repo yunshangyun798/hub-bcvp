fc17a2f8fd24b3ed06be159789dbb280eb0ef467
chaniann <129195278+chaniann@users.noreply.github.com>
2025-10-15 21:42:22 +0800

    Create 作业1.py

---
+++ 杨晨曦/week01/作业1.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import transformers
+import peft
+import sklearn
+import gensim
+import torch
+import matplotlib
+import pandas
+import numpy
+print('hello')
+print("Transformers version:", transformers.__version__)
+print("Peft version:", peft.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Gensim version:", gensim.__version__)
+print("PyTorch version:", torch.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Pandas version:", pandas.__version__)
+print("Numpy version:", numpy.__version__)
+
+print('Gg')
+
+#测试环境的代码
